c0d7ce3bf3b04d1c94233ddaefd9b17ca838bd92
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-12 00:05:31 +0530

    header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-gray-400'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <header className='p-4 h-24 bg-[#4F46E5] rounded-b-2xl text-white font-bold text-xl'>
+        <div className='name'>Ankit Kumar</div>
+        <nav className='navigation flex gap-4'>
+          <ul>Home</ul>
+          <ul>Projects</ul>
+        </nav>
+      </header>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
