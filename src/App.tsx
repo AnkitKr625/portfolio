4c7c4844d8b856b6cb30273aeef83bda4c086423
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-12 01:04:18 +0530

    header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Hero from './assets/hero.svg';
 import './App.css';
 
 function App() {
@@ -6,13 +7,19 @@ function App() {
 
   return (
     <>
-      <header className='p-4 h-24 bg-[#4F46E5] rounded-b-2xl text-white font-bold text-xl'>
+      <header className='p-4 h-24 bg-[#4F46E5] rounded-b-2xl text-white font-bold text-xl flex justify-between'>
         <div className='name'>Ankit Kumar</div>
-        <nav className='navigation flex gap-4'>
+        <nav className='navigation flex gap-4 mt-6'>
           <ul>Home</ul>
           <ul>Projects</ul>
         </nav>
       </header>
+      <main className='p-16 flex flex-col items-center'>
+        <section className='text-center'>
+          <img src={Hero} alt="hero" width={400}/>
+          <p className='font-semibold text-3xl'>Ankit Kumar <span className='text-[#4F46E5]'>a Software Developer</span></p>
+        </section>
+      </main>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
