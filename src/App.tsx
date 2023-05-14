8f721f5ab30d239e1a86b5c73403cb0475320562
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-14 23:54:19 +0530

    tech list added

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,23 @@ function App() {
           <ul>Projects</ul>
         </nav>
       </header>
-      <main className='p-16 flex flex-col gap-6 items-center'>
+      <main className='p-16 flex flex-col gap-12 items-center'>
         <section className='text-center'>
           <img src={Hero} alt="hero" width={400}/>
           <h1 className='font-semibold text-3xl'>Ankit Kumar <span className='text-[#4F46E5]'>a Software Developer</span></h1>
         </section>
-        <section className='tech-stack bg-[#F3F4F6]'>
-          <h1 className='font-semibold text-3xl'>Technologies</h1>
+        <section className='tech-stack bg-[#F3F4F6] w-full flex flex-col items-center'>
+          <h1 className='font-semibold text-3xl'>Technologies I have worked with:</h1>
+          <ul className='tech-stack-list '>
+            <li>JavasScript</li>
+            <li>React JS</li>
+            <li>Vue JS</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Git</li>
+            <li>Tailwind CSS</li>
+            <li>Bootstrap</li>
+          </ul>
         </section>
       </main>
       <footer></footer>
