b825be8af95766b91e542f87d0b8e2506758d937
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-16 23:03:52 +0530

    contanct section added

--- src/App.tsx
+++ src/App.tsx
@@ -32,9 +32,10 @@ function App() {
             </ul>
           </div>
         </section>
-        <section className='pitch py-8 w-full flex flex-col items-center'>
+        <section className='pitch py-8 w-full flex flex-col items-center gap-4'>
             <h1 className='font-semibold text-3xl'>Looking to Collaborate? Let's Talk</h1>
-            <p className='w-1/2 pl-4'>I enjoy working on UI, and I can create a beautiful UI for you. I'm eager to work on the frontend component of it in partnership with a business or individual. Additionally, I'm interested in freelancing. Let's discuss more!</p>
+            <p className='w-full md:w-1/2 md:pl-4'>I enjoy working on UI, and I can create a beautiful UI for you. I'm eager to work on the frontend component of it in partnership with a business or individual. Additionally, I'm interested in freelancing. Let's discuss more!</p>
+            <a href='mailto:ankitkr1009@gmail.com'>ankitkr1009@gmail.com</a>
         </section>
       </main>
       <footer></footer>
