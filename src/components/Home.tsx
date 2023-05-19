a1f6c8604d3aeb43bab0386623208da6d2e28497
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-19 23:34:47 +0530

    project added in home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -23,10 +23,15 @@ function Home() {
             </ul>
           </div>
         </section>
-        <section className='pitch py-8 w-full flex flex-col items-center gap-4'>
+        <section className='projects py-8 w-full flex flex-col items-center gap-4'>
+          <h1 className='font-semibold text-3xl'>Projects</h1>
+          <p className='w-full md:w-1/2 md:pl-4 text-xl'>I like to showcase my work and thus, you can see my projects hosted online.</p>
+          <button className='project bg-[#4F46E5] px-4 py-2 rounded-lg text-white text-xl font-semibold'>Browse Projects</button>
+        </section>
+        <section className='pitch bg-[#F3F4F6] py-8 w-full flex flex-col items-center gap-4'>
             <h1 className='font-semibold text-3xl'>Looking to Collaborate? Let's Talk</h1>
             <p className='w-full md:w-1/2 md:pl-4 text-xl'>I enjoy working on UI, and I can create a beautiful UI for you. I'm eager to work on the frontend component of it in partnership with a business or individual. Additionally, I'm interested in freelancing. Let's discuss more!</p>
-            {/* <a href='mailto:ankitkr1009@gmail.com'>ankitkr1009@gmail.com</a> */}
+            <a className='cursor-pointer' href='mailto:ankitkr1009@gmail.com'>ankitkr1009@gmail.com</a>
         </section>
     </>
   )
