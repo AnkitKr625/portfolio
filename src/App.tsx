6450c2f41c5d192c225f66237f2dd0f96c110159
Ankit Kumar <ankitkr1009@gmail.com>
2023-05-20 13:42:08 +0530

    twitter added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from './components/Home.tsx';
 import GitHubLogo from './assets/github.svg';
 import LinkedInLogo from './assets/linkedIn.svg';
 import MailLogo from './assets/mail.svg';
+import TwitterLogo from './assets/twiiter.svg';
 import './App.css';
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
       <main className='p-16 flex flex-col items-center'>
         <Home/>
       </main>
-      <footer className='p-4 h-24 bg-[#4F46E5] rounded-t-2xl text-white font-bold text-xl flex items-center justify-center gap-4'>
+      <footer className='p-4 h-24 bg-[#4F46E5] rounded-t-2xl text-white font-bold text-xl flex items-center justify-center gap-8'>
         <img
           src={GitHubLogo}
           alt="git-hub"
@@ -41,6 +42,13 @@ function App() {
           className='cursor-pointer'
           width={40}
           onClick={() => {window.open('https://www.linkedin.com/in/ankitkr625/', '_blank')}}
+        />
+        <img
+          src={TwitterLogo}
+          alt="twitter"
+          className='cursor-pointer'
+          width={40}
+          onClick={() => {window.open('https://twitter.com/AnkitKr625', '_blank')}}
         />
       </footer>
     </>
